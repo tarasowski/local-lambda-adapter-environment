605f1077949b42aa12aaa4f503e7dc45a7d18c72
Dimitri Tarasowski <33748291+tarasowski@users.noreply.github.com>
2018-08-11 08:31:43 +0200

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -2,4 +2,12 @@ const config = require('./config.json')
 const environment = require('./environment')
 const save = require(`${environment(config)}`).save
 
-save()
+module.exports.handler = async(event) => {
+    try { 
+        await save(event)
+        return 'Works'
+    } catch (e) {
+        console.log(e.message)
+        return 'Something went wrong'
+    }
+}
